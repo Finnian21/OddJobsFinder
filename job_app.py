f916d830c61f2046b934eaf9eb5da65d4f21c027
Finnian <finnian2010@hotmail.com>
2019-02-25 23:31:56 +0000

    new commit

--- job_app.py
+++ job_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, escape, request, redirect
+from flask import Flask, render_template, url_for, escape, request, redirect, session
 import datetime
 import pymysql
 
@@ -6,14 +6,45 @@ db = pymysql.connect(host='localhost', user='root', passwd='', db = 'casualJobs3
 app = Flask(__name__)
 cursor = db.cursor()
 
-user_id = "3"
+@app.route('/', methods = ['GET', 'POST'])
+def login():
+    error = None
+
+    if request.method == 'POST':
+
+        first_name = request.form['firstName']
+        last_name = request.form['lastName']
+        password = request.form['password']
+        sql = "SELECT * from users where firstName='" + first_name + "' and lastName='" + last_name + "" + "' and password='" + password + "'"
+
+        cursor.execute(sql)
+
+        if cursor.fetchone() is None:
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            session['username'] = request.form['firstName'] + " " + request.form['lastName']
+            return redirect("/home", code=302)
+
+    return render_template('login.html', error=error)
 
 @app.route('/post_job', methods = ['GET', 'POST'])
 def post_job():
-
-    sql = "SELECT firstName, lastName FROM users where userId =" + user_id
+    
+    if 'username' in session:
+        username = session['username']
+    
+    the_username = username.split()
+    
+    sql = "SELECT * FROM users where firstName ='" + the_username[0]  + "' and lastName='" + the_username[1] + "'"
     cursor.execute(sql)
     results = cursor.fetchall()
+    
+    user_Id = ''
+
+    for row in results:
+        user_Id = row[0]
+
+    the_user_Id = str(user_Id)
 
     if request.method == 'POST':
         title =  request.form["inputTitle"]
@@ -37,8 +68,8 @@ def post_job():
             phone = request.form["phone"]
             email = request.form["email"]    
         else:
-            sql1 = "SELECT phone, email FROM users where userId =" + user_id
-            cursor.execute(sql1)
+            sql1 = "SELECT phone, email FROM users where userId =" + the_user_Id
+            cursor.execute(sql)
             results2 = cursor.fetchall()
             
             for row in results2:
@@ -51,7 +82,7 @@ def post_job():
             county = request.form["county"] 
  
         else:
-            sql2 = "SELECT street, town, county FROM users where userId =" + user_id
+            sql2 = "SELECT street, town, county FROM users where userId =" + the_user_Id
             cursor.execute(sql2)
             results3 = cursor.fetchall()
             
@@ -62,32 +93,49 @@ def post_job():
         
         time_stamp_posted = datetime.datetime.now()
 
-        cursor.execute("INSERT INTO jobs (title, UserID, description, duration, pay, catagory, timeStampPosted, resourcesProvided, resourcesRequired, email, phone, street, town, county) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (title, user_id, description, duration, pay, catagory, time_stamp_posted, resources_provided, resources_required, email, phone, street, town, county))
+        cursor.execute("INSERT INTO jobs (title, UserID, description, duration, pay, catagory, timeStampPosted, resourcesProvided, resourcesRequired, email, phone, street, town, county) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (title, the_user_Id, description, duration, pay, catagory, time_stamp_posted, resources_provided, resources_required, email, phone, street, town, county))
         db.commit()
 
-        cursor.execute("UPDATE users SET JobsPosted = JobsPosted + 1 WHERE userID = %s", (user_id))
+        cursor.execute("UPDATE users SET JobsPosted = JobsPosted + 1 WHERE userID = %s", (the_user_Id))
         db.commit()
 
-        return redirect("/view_job", code=302)
+        return redirect("/view_jobs", code=302)
 
     return render_template('postJob.html', results=results)
 
 @app.route('/view_jobs', methods = ['GET', 'POST'])
-def view_job():
-        rowCount = '9'
+def view_jobs():
+        if 'username' in session:
+            username = session['username']
+    
+        the_username = username.split()
+    
+        sql = "SELECT * FROM users where firstName ='" + the_username[0]  + "' and lastName='" + the_username[1] + "'"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+    
+        user_Id = ''
+
+        for row in results:
+            the_user_Id = row[0]
+
         sql2 = "SELECT * FROM jobs INNER JOIN users ON jobs.UserID=users.userId ORDER BY timeStampPosted DESC"
         cursor.execute(sql2)
         results2 = cursor.fetchall()
-        selected_animal = request.args.get('type')
-        return render_template('viewJob.html', results2 = results2)
+
+        return render_template('viewJob.html', results2 = results2, the_user_Id = the_user_Id)
 
 @app.route('/home', methods = ['GET', 'POST'])
 def home():
-        return render_template('home.html')
+        if 'username' in session:
+            username = session['username']
+        return render_template('home.html', username = username)
 
-@app.route('/login', methods = ['GET', 'POST'])
-def login():
-        return render_template('login.html')
+@app.route('/log_out', methods = ['GET', 'POST'])
+def log_out():
+        session.pop('username', None)
+        return redirect("/", code=302)
 
+app.secret_key = 'super secret key'
 if __name__ == '__main__':
    app.run(debug = True)
