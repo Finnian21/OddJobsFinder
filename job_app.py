8d65b9d8987eed5ebd5598f260d6879dc0e16f54
Finnian <finnian2010@hotmail.com>
2019-03-18 19:54:51 +0000

    message

--- job_app.py
+++ job_app.py
@@ -229,6 +229,7 @@ def register():
         password = request.form["password"]
 
         session['username'] = username
+        session['userType'] = user_type
 
         cursor.execute("INSERT INTO users (firstName, lastName, username, userType, description, age, phone, email, street, town, county, password) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (firstname, lastname, username, user_type, description, age, phone, email, street, town, county, password))
         db.commit()
