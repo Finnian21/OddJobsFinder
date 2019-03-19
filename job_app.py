377f7277f229eb3c8a799b7a1488d211c44de7bb
Finnian <finnian2010@hotmail.com>
2019-03-19 20:45:32 +0000

    message

--- job_app.py
+++ job_app.py
@@ -249,7 +249,7 @@ def edit_job():
         sql = "SELECT * FROM jobs WHERE jobId = " + job_id
         cursor.execute(sql)
         results = cursor.fetchall()
-        
+        """
         if request.method == 'POST':
             title =  request.form["inputTitle"]
             description =  request.form["description"]
@@ -263,12 +263,12 @@ def edit_job():
             street = request.form["street"]
             town = request.form["town"]
             county = request.form["county"]
-
+        """
             cursor.execute("""UPDATE jobs SET title = %s, description = %s, duration = %s, pay = %s, catagory = %s, resourcesProvided = %s,
             resourcesRequired = %s, email = %s, phone = %s, email = %s, street = %s, town = %s, county = %s""", (title, description, duration, pay, 
             catagory, resources_provided, resources_required, phone, email, street, town, county))
             db.commit()
-
+    
             return redirect("/view_job", code=302)
 
         return render_template('editJob.html', results=results)
