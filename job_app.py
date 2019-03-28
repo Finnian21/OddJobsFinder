4bbbe1a6abbcb063cc1cfb856d8e527f8e500735
Finnian <finnian2010@hotmail.com>
2019-03-28 17:27:12 +0000

    message

--- job_app.py
+++ job_app.py
@@ -363,8 +363,21 @@ def accept_user():
     results2 = cursor.fetchall()
 
     for row in results2:
-        print(row[1])
-        print(row[2])
+        decline_user_id = row[2]
+
+        sql = "SELECT * FROM users where userId ='" + decline_user_id + "'"
+        cursor.execute(sql)
+        results3 = cursor.fetchall()
+        title = session['title']
+
+        for row in results3:
+            email = row[8]
+            firstname = row[1]
+
+            msg = Message('Job Taken', sender = 'oddjobsfinder@gmail.com', recipients = [email])
+            msg.body = "Hi, you have been accepted."
+            msg.html = render_template("/decline.html", title = title, job_username = job_username, firstname=firstname)
+            mail.send(msg)
 
     cursor.close()
     db.close()
